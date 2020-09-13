0aa90e323cfbf167d3620140adcb433972ead8f1
g2t3s <gerardo.tejada.saracho@gmail.com>
2020-09-13 23:33:23 +0300

    First commit - Spring Initilizr

---
+++ src/main/java/com/eestienergia/BakeSaleManager/BakeSaleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.eestienergia.BakeSaleManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeSaleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeSaleManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eestienergia/BakeSaleManager/BakeSaleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eestienergia.BakeSaleManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeSaleManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
